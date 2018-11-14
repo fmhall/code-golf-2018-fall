51e366621e7c023a43ac9aec7887c2bf2d0b7869
Mason Hall <masonhall@gmail.com>
2018-11-13 20:54:16 -0600

    3rd problem eh

--- solutions/prob2.py
+++ solutions/prob2.py
@@ -1,1 +1,1 @@
-for i in (x for x in range(1,999999)if(str(x)[-1]==str(x)[0]and(x%3==0 or x%5==0 or x%7==0))):print(i)
+for i in(x for x in range(1,999999)if(str(x)[-1]==str(x)[0]and(x%3==0 or x%5==0 or x%7==0))):print(i)
